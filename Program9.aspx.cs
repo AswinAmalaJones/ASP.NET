0974f108cfee3472ea85907a99be411aed014533
AswinAmalaJones <aswinamalajones2005@gmail.com>
2026-04-05 20:38:34 +0530

    Create Program9.aspx.cs

---
+++ Program9.aspx.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace program9
+{
+    public partial class Program9 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
