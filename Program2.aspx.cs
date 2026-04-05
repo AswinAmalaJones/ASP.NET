4930867eed89c03f9df3efc15d1df6cf7ee92c07
AswinAmalaJones <aswinamalajones2005@gmail.com>
2026-04-05 20:24:27 +0530

    Add Program2.aspx.cs for greeting functionality

---
+++ Program2.aspx.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace program2
+{
+    public partial class Program2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                string name = Request.Form["name"];
+                Response.Write("Hello, " + name);
+            }
+        }
+    }
+}
