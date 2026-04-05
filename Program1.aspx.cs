02bc32ad7a87deaa0d33d8db46028f61b25ea197
AswinAmalaJones <aswinamalajones2005@gmail.com>
2026-04-05 19:15:24 +0530

    Add Page_Load method to display browser info

---
+++ Program1.aspx.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace program1
+{
+    public partial class Program1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblInfo.Text =
+                "This web application is running on: " +
+                Request.Browser.Browser + " " +
+                Request.Browser.Version +
+                "<br/>Server software: ASP.NET using Visual Studio 2019" +
+                "<br/>Tools used: Visual Studio, .NET Framework, IIS Express.";
+        }
+    }
+}
