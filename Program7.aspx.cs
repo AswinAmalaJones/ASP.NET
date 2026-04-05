546349608fb7d89590070b3260b043984bce5d19
AswinAmalaJones <aswinamalajones2005@gmail.com>
2026-04-05 20:34:01 +0530

    Add initial Program7.aspx.cs file with Page_Load

---
+++ Program7.aspx.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace program7
+{
+    public partial class Program7 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
